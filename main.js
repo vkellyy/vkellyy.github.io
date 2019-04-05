9f70c8d93783d204e844e878a1777315528607b8
Joseph Gaughan <47546055+Shoneys@users.noreply.github.com>
2019-04-04 21:43:14 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 //requires jquery and javascript
 $(document).ready(function () {
-    $('body').removeAttr('id');
-
     function displayTime() {
+
         var countDownDate = new Date("April 17, 2029 15:00:00").getTime();
 
         var x = setInterval(function () {
-
-
             var now = new Date().getTime();
-
-
             var distance = countDownDate - now;
-
 
             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
             var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -20,15 +14,11 @@ $(document).ready(function () {
             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
             if (seconds < 10) {
-                // Add the "0" digit to the front
-                // so 9 becomes "09"
                 seconds = "0" + seconds;
             }
-            // If the minutes digit is less than ten
             if (minutes < 10) {
                 minutes = "0" + minutes;
             }
-            // If the hours digit is less than ten
             if (hours < 10) {
                 hours = "0" + hours;
             }
