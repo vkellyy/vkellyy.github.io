28c85d66d1f9393cf6457be0e52df82b0b85afb4
Shoneys <jrgaughan1@gmail.com>
2019-04-04 21:22:33 -0400

    major changes
    changes to folder structure
    menu is now external html doc
    added placeholder border for when isaac gives me transparent icons

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
-var countDownDate = new Date("April 17, 2029 15:00:00").getTime();
+//requires jquery and javascript
+$(document).ready(function () {
+    function displayTime() {
+        var countDownDate = new Date("April 17, 2029 15:00:00").getTime();
 
-var x = setInterval(function () {
-
-
-    var now = new Date().getTime();
-
-
-    var distance = countDownDate - now;
+        var x = setInterval(function () {
 
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            var now = new Date().getTime();
 
-    if (seconds < 10) {
-        // Add the "0" digit to the front
-        // so 9 becomes "09"
-        seconds = "0" + seconds;
+
+            var distance = countDownDate - now;
+
+
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            if (seconds < 10) {
+                // Add the "0" digit to the front
+                // so 9 becomes "09"
+                seconds = "0" + seconds;
+            }
+            // If the minutes digit is less than ten
+            if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+            // If the hours digit is less than ten
+            if (hours < 10) {
+                hours = "0" + hours;
+            }
+            //if days=0, show docs
+
+            document.getElementById("capsule").innerHTML = days + ":" + hours + ":" +
+                minutes + ":" + seconds + "";
+        });
     }
-    // If the minutes digit is less than ten
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    // If the hours digit is less than ten
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
-
-    document.getElementById("capsule").innerHTML = days + ":" + hours + ":" +
-        minutes + ":" + seconds + "";
-
-
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("capsule").innerHTML = "-";
-    }
-}, 1000);
+    displayTime();
+});
