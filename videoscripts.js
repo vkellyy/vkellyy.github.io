41083fa8c63c9487ebaf105420788b12a6c69d8a
Joseph Gaughan <jrgaughan1@gmail.com>
2019-04-16 22:25:08 -0400

    uploaded some videos

--- videoscripts.js
+++ videoscripts.js
@@ -8,4 +8,37 @@ $(document).ready(function () {
     $('#crib').click(function () {
         $("#mediaplayer").attr("src", "GenXvideos/crib.mp4");
     });
+    $('#salty').click(function () {
+        $("#mediaplayer").attr("src", "GenZvideos/salty.mp4");
+    });
+    $('#shook').click(function () {
+        $("#mediaplayer").attr("src", "GenZvideos/shook.mp4");
+    });
+    $('#bussin').click(function () {
+        $("#mediaplayer").attr("src", "GenZvideos/bussin.mp4");
+    });
+    $('#thirsty').click(function () {
+        $("#mediaplayer").attr("src", "GenZvideos/thirsty.mp4");
+    });
+    $('#jakes').click(function () {
+        $("#mediaplayer").attr("src", "GenZvideos/jakes.mp4");
+    });
+    $('#clipped').click(function () {
+        $("#mediaplayer").attr("src", "GenZvideos/clipped.mp4");
+    });
+    $('#jawn').click(function () {
+        $("#mediaplayer").attr("src", "GenZvideos/jawn.mp4");
+    });
+    $('#jit').click(function () {
+        $("#mediaplayer").attr("src", "GenZvideos/jit.mp4");
+    });
+    $('#big_smoke').click(function () {
+        $("#mediaplayer").attr("src", "GenZvideos/big_smoke.mp4");
+    });
+    $('#bop').click(function () {
+        $("#mediaplayer").attr("src", "GenZvideos/bop.mp4");
+    });
+    $('#deep').click(function () {
+        $("#mediaplayer").attr("src", "GenZvideos/deep.mp4");
+    });
 });
